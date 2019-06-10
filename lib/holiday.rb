3f98e5442603b8d50c97183c7f700e04701d2cf8
Chris Lincoln <chris.lincoln12@gmail.com>
2019-06-10 13:33:16 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -30,7 +30,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   holiday_hash.each do |season, holiday|
     if season == :winter
       season.each do |holiday, item|
-        supply 
+        supply << item
         
       end
     end
