a6546e67c0a7fffe555e603d47a939939905eaad
Chris Lincoln <chris.lincoln12@gmail.com>
2019-06-10 17:19:55 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -76,7 +76,7 @@ def all_holidays_with_bbq(holiday_hash)
   
   holiday_hash.map do |season, holiday|
     holiday_hash[season].map do |holiday, items|
-      holiday.flatten.compact if items.include?("BBQ")
+      holiday if items.include?("BBQ")
     end
   end
 end
