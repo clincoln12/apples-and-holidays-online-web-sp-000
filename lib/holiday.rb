18e40b6ca7c0ba8ee268b479e655fd16b189da51
Chris Lincoln <chris.lincoln12@gmail.com>
2019-06-10 16:14:55 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,7 +67,7 @@ def all_supplies_in_holidays(holiday_hash)
       holiday.each do { |x| x.capitalize }
       holiday.join
       puts "  #{holiday.capitalize}: #{items.join(", ")}"
-    end
+    
     end
   end
 end
